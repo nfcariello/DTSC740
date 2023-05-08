2b6be16e7122524426107e5b0f203474e287910d
nfcariello <ncariell@nyit.edu>
2023-05-08 19:41:25 -0400

    project2

--- project2/project2.py
+++ project2/project2.py
@@ -6,205 +6,183 @@ import time
 
 from IPython import display
 
-# Baseline
-optimizer = 'baseline'
-run = 'baseline'
-relu_state = 'leaky'
-noise_dim = 100
-BATCH_SIZE = 256
 
-# Normal ReLU
-# optimizer = 'baseline'
-# run = 'relu'
-# relu_state = 'normal'
-# noise_dim = 100
-# BATCH_SIZE = 256
+def dcgan(optimizer, run, relu_state, noise_dim, BATCH_SIZE):
+    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
-# Custom Settings
-# optimizer = 'custom'
-# run = 'custom'
-# relu_state = 'leaky'
-# noise_dim = 128
-# BATCH_SIZE = 128
+    BUFFER_SIZE = 60000
 
+    # Batch and shuffle the data
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
 
-(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+    def make_generator_model():
+        model = tf.keras.Sequential()
+        model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(noise_dim,)))
+        model.add(layers.BatchNormalization())
+        if relu_state == 'normal':
+            model.add(layers.ReLU())
+        else:
+            model.add(layers.LeakyReLU())
 
-BUFFER_SIZE = 60000
+        model.add(layers.Reshape((7, 7, 256)))
+        assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
 
-# Batch and shuffle the data
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+        model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+        assert model.output_shape == (None, 7, 7, 128)
+        model.add(layers.BatchNormalization())
+        if relu_state == 'normal':
+            model.add(layers.ReLU())
+        else:
+            model.add(layers.LeakyReLU())
 
+        model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+        assert model.output_shape == (None, 14, 14, 64)
+        model.add(layers.BatchNormalization())
+        if relu_state == 'normal':
+            model.add(layers.ReLU())
+        else:
+            model.add(layers.LeakyReLU())
 
-def make_generator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(noise_dim,)))
-    model.add(layers.BatchNormalization())
-    if relu_state == 'normal':
-        model.add(layers.ReLU())
+        model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+        assert model.output_shape == (None, 28, 28, 1)
+
+        return model
+
+    generator = make_generator_model()
+
+    noise = tf.random.normal([1, noise_dim])
+    generated_image = generator(noise, training=False)
+
+    plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+
+    def make_discriminator_model():
+        model = tf.keras.Sequential()
+        model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                input_shape=[28, 28, 1]))
+        if relu_state == 'normal':
+            model.add(layers.ReLU())
+        else:
+            model.add(layers.LeakyReLU())
+        model.add(layers.Dropout(0.3))
+
+        model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+        if relu_state == 'normal':
+            model.add(layers.ReLU())
+        else:
+            model.add(layers.LeakyReLU())
+        model.add(layers.Dropout(0.3))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(1))
+
+        return model
+
+    discriminator = make_discriminator_model()
+    decision = discriminator(generated_image)
+
+    # This method returns a helper function to compute cross entropy loss
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    def discriminator_loss(real_output, fake_output):
+        real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+        fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+    def generator_loss(fake_output):
+        return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+    if optimizer == 'baseline':
+        generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+        discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
     else:
-        model.add(layers.LeakyReLU())
+        generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
+        discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                     discriminator_optimizer=discriminator_optimizer,
+                                     generator=generator,
+                                     discriminator=discriminator)
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
-    model.add(layers.BatchNormalization())
-    if relu_state == 'normal':
-        model.add(layers.ReLU())
-    else:
-        model.add(layers.LeakyReLU())
+    EPOCHS = 100
+    num_examples_to_generate = 16
 
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
-    model.add(layers.BatchNormalization())
-    if relu_state == 'normal':
-        model.add(layers.ReLU())
-    else:
-        model.add(layers.LeakyReLU())
+    # You will reuse this seed overtime (so it's easier)
+    # to visualize progress in the animated GIF)
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
+    # Notice the use of `tf.function`
+    # This annotation causes the function to be "compiled".
+    @tf.function
+    def train_step(images):
+        noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
-    return model
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = generator(noise, training=True)
 
+            real_output = discriminator(images, training=True)
+            fake_output = discriminator(generated_images, training=True)
 
-generator = make_generator_model()
+            gen_loss = generator_loss(fake_output)
+            disc_loss = discriminator_loss(real_output, fake_output)
 
-noise = tf.random.normal([1, noise_dim])
-generated_image = generator(noise, training=False)
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
-plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
+    def generate_and_save_images(model, epoch, test_input):
+        # Notice `training` is set to False.
+        # This is so all layers run in inference mode (batchnorm).
+        predictions = model(test_input, training=False)
 
-def make_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                            input_shape=[28, 28, 1]))
-    if relu_state == 'normal':
-        model.add(layers.ReLU())
-    else:
-        model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+        fig = plt.figure(figsize=(4, 4))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    if relu_state == 'normal':
-        model.add(layers.ReLU())
-    else:
-        model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+        for i in range(predictions.shape[0]):
+            plt.subplot(4, 4, i + 1)
+            plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+            plt.axis('off')
 
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+        plt.savefig(run + '_image_at_epoch_{:04d}.png'.format(epoch))
+        plt.show()
 
-    return model
+    def train(dataset, epochs):
+        for epoch in range(epochs):
+            start = time.time()
 
+            for image_batch in dataset:
+                train_step(image_batch)
 
-discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
+            # Produce images for the GIF as you go
+            display.clear_output(wait=True)
+            generate_and_save_images(generator,
+                                     epoch + 1,
+                                     seed)
 
-# This method returns a helper function to compute cross entropy loss
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+            # Save the model every 10 epochs
+            if (epoch + 1) % 10 == 0:
+                checkpoint.save(file_prefix=checkpoint_prefix)
 
+            print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
 
-def discriminator_loss(real_output, fake_output):
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
-    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
-    total_loss = real_loss + fake_loss
-    return total_loss
-
-
-def generator_loss(fake_output):
-    return cross_entropy(tf.ones_like(fake_output), fake_output)
-
-
-if optimizer == 'baseline':
-    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-else:
-    generator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
-    discriminator_optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002, beta_1=0.5)
-
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
-
-EPOCHS = 100
-num_examples_to_generate = 16
-
-# You will reuse this seed overtime (so it's easier)
-# to visualize progress in the animated GIF)
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
-
-
-# Notice the use of `tf.function`
-# This annotation causes the function to be "compiled".
-@tf.function
-def train_step(images):
-    noise = tf.random.normal([BATCH_SIZE, noise_dim])
-
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        generated_images = generator(noise, training=True)
-
-        real_output = discriminator(images, training=True)
-        fake_output = discriminator(generated_images, training=True)
-
-        gen_loss = generator_loss(fake_output)
-        disc_loss = discriminator_loss(real_output, fake_output)
-
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
-
-
-def generate_and_save_images(model, epoch, test_input):
-    # Notice `training` is set to False.
-    # This is so all layers run in inference mode (batchnorm).
-    predictions = model(test_input, training=False)
-
-    fig = plt.figure(figsize=(4, 4))
-
-    for i in range(predictions.shape[0]):
-        plt.subplot(4, 4, i + 1)
-        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
-        plt.axis('off')
-
-    plt.savefig(run + '_image_at_epoch_{:04d}.png'.format(epoch))
-    plt.show()
-
-
-def train(dataset, epochs):
-    for epoch in range(epochs):
-        start = time.time()
-
-        for image_batch in dataset:
-            train_step(image_batch)
-
-        # Produce images for the GIF as you go
+        # Generate after the final epoch
         display.clear_output(wait=True)
         generate_and_save_images(generator,
-                                 epoch + 1,
+                                 epochs,
                                  seed)
 
-        # Save the model every 10 epochs
-        if (epoch + 1) % 10 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
-
-        print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
-
-    # Generate after the final epoch
-    display.clear_output(wait=True)
-    generate_and_save_images(generator,
-                             epochs,
-                             seed)
+    train(train_dataset, EPOCHS)
 
 
-train(train_dataset, EPOCHS)
+if __name__ == '__main__':
+    # Run the DCGAN with the baseline settings
+    dcgan('baseline', 'baseline', 'leaky', 100, 256)
+    # Run the DCGAN with a normal ReLU function instead of leaky
+    dcgan('baseline', 'relu', 'normal', 100, 256)
+    # Run the DCGAN with the custom hyperparameters set
+    dcgan('custom', 'custom', 'leaky', 128, 128)
---
+++ project2/readme.md
@@ -0,0 +1,11 @@
+# Project 2
+
+This project was built in `Python 3.8`
+
+## Getting Started
+1. Install dependencies after loading into a virtual environment: `pip3 install -r requirements.txt`
+2. Run the main function which runs DCGAN with the baseline, normal ReLU, and custom hyperparameters
+
+## DCGAN
+
+https://www.tensorflow.org/tutorials/generative/dcgan
